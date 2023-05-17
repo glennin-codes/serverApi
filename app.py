3eca07b7083a9707381ea660d053bf5e7e41f026
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-05-17 06:26:47 +0300

    complete pl matches predictions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+
 import pandas as pd
 import requests
 from flask import Flask, request, jsonify
@@ -8,6 +8,7 @@ import logging
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
+from flask_cors import CORS
 
 # Fetch the data from football-data.org API
 url = "https://api.football-data.org/v4/competitions/PL/matches?status=FINISHED"
@@ -64,6 +65,8 @@ print(f"Model trained successfully with accuracy score: {accuracy}")
 # Build the REST API with Flask
 app = Flask(__name__)
 
+CORS(app, origins="*")
+
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
@@ -72,6 +75,53 @@ def predict():
     prediction = model.predict([[home_team_id, away_team_id]])
     logging.info("Prediction made successfully")
     return jsonify({"prediction": int(prediction.argmax(axis=-1))})
+
+# Define endpoint for fetching scheduled matches
+@app.route('/matches', methods=['GET'])
+def get_scheduled_matches():
+    # Call the football-data.org API to get the list of scheduled matches
+    response = requests.get('https://api.football-data.org/v4/matches?status=SCHEDULED',
+                            headers={'X-Auth-Token': '5777078bb3ca4a72a3e01a5fdebac8db'})
+    
+    # If the API call is successful, parse the response and return the data
+    if response.status_code == 200:
+        # Extract the list of matches from the response
+        matches = response.json()['matches']
+        
+        # Define an empty list to hold the modified data
+        data = []
+        
+        # Loop through the list of matches and extract the relevant information
+        for match in matches:
+            # Extract the relevant information from the match object
+            match_id=match['id']
+            home_team_id = match['homeTeam']['id']
+            home_team_name = match['homeTeam']['name']
+            home_team_logo = match["homeTeam"]["crest"]
+            away_team_id = match['awayTeam']['id']
+            away_team_name = match['awayTeam']['name']
+            away_team_logo = match["awayTeam"]["crest"]
+            match_time = match['utcDate']
+           
+            # Add the extracted information to the data list
+            data.append({
+                'matchId' : match_id,
+                'homeTeamId': home_team_id,
+                'homeTeamName': home_team_name,
+                'homeTeamLogo': home_team_logo,
+                'awayTeamId': away_team_id,
+                'awayTeamName': away_team_name,
+                'awayTeamLogo': away_team_logo,
+                'matchTime': match_time
+            })
+        
+        # Return the data list as a JSON object
+        return jsonify(data)
+    
+    # If the API call is unsuccessful, return an error message
+    else:
+        return jsonify({'error': 'Failed to fetch scheduled matches.'}), response.status_code
+
 
 
 # Run the Flask app
